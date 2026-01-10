379bd39b848a0eef3dceebf258ac5b63939482f9
Anondo Ray <anondo554@gmail.com>
2026-01-10 22:27:14 +0600

    pojecet setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>
+        <Button>Click</Button>
+      </h1>
+    </>
+  );
+}
+
+export default App;
